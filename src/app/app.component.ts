09cd93b06b7fdd77f3ed270a3f9811a3cf9cfc1d
Alexandre Vermeersch <112808930+AlexVerm12@users.noreply.github.com>
2023-01-02 11:08:18 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Firestore, collectionData, collection } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'firebase-todolist';
+  todos$: Observable<any>;
+  todos: Array<any>;
+  todotext:'' ;
+  constructor(private firestore: Firestore) {
+    const coll = collection(firestore, 'todos');
+    this.todos$ = collectionData(coll);
+
+    this.todos$.subscribe((newTodos) => {
+      console.log('Neue Todos sind:', newTodos)
+      this.todos = newTodos;
+    });
+  }
+
+  addTodo(){
+    const coll = collection(this.firestore, 'todos');
+    setDoc(doc(coll, "your-id"), {name: this.todotext});
+  }
 }
+function setDoc(arg0: any, infos: any) {
+  throw new Error('Function not implemented.');
+}
+
+function doc(notesRef: any, arg1: string): any {
+  throw new Error('Function not implemented.');
+}
+
---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  firebase: {
+    projectId: 'alexvermtodolist',
+    appId: '1:746659752528:web:2abee7e24645ec89007f55',
+    storageBucket: 'alexvermtodolist.appspot.com',
+    apiKey: 'AIzaSyDjL1vymjxowaHwhTbpCgGrAlL7ec0ASDQ',
+    authDomain: 'alexvermtodolist.firebaseapp.com',
+    messagingSenderId: '746659752528',
+  },
+    production: false,
+}
